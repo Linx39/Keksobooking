a9aa2bcfc521e31c2577c27093f49cba878b74d4
Linx <olga-reshetova@mail.ru>
2022-04-18 13:51:08 +0700

    исправлена ошибка валидации поля цена

--- js/main.js
+++ js/main.js
@@ -1,7 +1,2 @@
-// import './popup.js';
-// import { createPopup } from './popup.js';
-// const mapCanvas = document.querySelector('.map__canvas');
-// const popups = createPopup();
-// mapCanvas.appendChild(popups.children[0]);
 import './notice.js';
 import './map-download.js';
