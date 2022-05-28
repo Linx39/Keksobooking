26f07280dd6f4511118e16d1521b49d9e4b3a7d5
Linx <olga-reshetova@mail.ru>
2022-05-28 17:15:26 +0700

    фильтрация почти сделана, остались детали

--- source/js/filters.js
+++ source/js/filters.js
@@ -16,124 +16,82 @@ const housingType = mapFilters.querySelector('#housing-type');
 const housingPrice = mapFilters.querySelector('#housing-price');
 const housingRooms = mapFilters.querySelector('#housing-rooms');
 const housingGuests = mapFilters.querySelector('#housing-guests');
-const filterWifi = mapFilters.querySelector('#filter-wifi');
-const filterDishwasher = mapFilters.querySelector('#filter-dishwasher');
-const filterParking = mapFilters.querySelector('#filter-parking');
-const filterWasher = mapFilters.querySelector('#filter-washer');
-const filterElevator = mapFilters.querySelector('#filter-elevator');
-const filterConditioner = mapFilters.querySelector('#filter-conditioner');
+const filterFeatures = mapFilters.querySelectorAll('input[type="checkbox"]');
 
+//Функция, отслеживающая изменения фильтров
 const onFormMapFiltersChange = (cb) => {
-  mapFilters.addEventListener('change', (evt) => {
-    console.log(evt.target);
-    cb();
-  });
+  mapFilters.addEventListener('change', () => cb());
 };
-
-
-//Функции, отслеживающие изменения фильтров
-// const onHousingTypeChange = (cb) => {
-//   housingType.addEventListener('change', () => cb())
-// };
-
-// const onHousingPriceChange = (cb) => {
-//   housingPrice.addEventListener('change', () => cb())
-// };
-
-// const onHousingRoomsChange = (cb) => {
-//   housingRooms.addEventListener('change', () => cb())
-// };
-
-// const onHousingGuestsChange = (cb) => {
-//   housingGuests.addEventListener('change', () => cb())
-// };
-
-// const onFilterWifiClick = (cb) => {
-//   filterWifi.addEventListener('click', () => cb())
-// };
-
-// const onFilterDishwasherClick  = (cb) => {
-//   filterDishwasher.addEventListener('click', () => cb())
-// };
-
-// const onFilterParkingClick  = (cb) => {
-//   filterParking.addEventListener('click', () => cb())
-// };
-
-// const onFilterWasherClick  = (cb) => {
-//   filterWasher.addEventListener('click', () => cb())
-// };
-
-// const onFilterElevatorClick  = (cb) => {
-//   filterElevator.addEventListener('click', () => cb())
-// };
-
-// const onFilterConditionerClick  = (cb) => {
-//   filterConditioner.addEventListener('click', () => cb())
-// };
 
 //Функция, определяющая правила фильтрация попапов
 const filterPopup = ({offer}) => {
   const {price, type, rooms, guests, features} = offer;
 
-  const isHousingType = housingType.value === Default_Value.HOUSING_TYPE || housingType.value === type;
-
-  let priceValue;
-  if (price < Price_Rank.LOW[1]) {
-    priceValue = Price_Rank.LOW[0];
-  }
-  if (price >= Price_Rank.LOW[1] && price <= Price_Rank.MIDDLE[1]) {
-    priceValue = Price_Rank.MIDDLE[0];
-  }
-  if (price > Price_Rank.MIDDLE[1]) {
-    priceValue = Price_Rank.HIGH[0];
-  }
-  const isHousingPrice = housingPrice.value === Default_Value.HOUSING_PRICE || housingPrice.value === priceValue;
-
-  const isHousingRooms = housingRooms.value === Default_Value.HOUSING_ROOMS || housingRooms.value === String(rooms);
-
-  const isHousingGuests = housingGuests.value === Default_Value.HOUSING_GUEST || housingGuests.value === String(guests);
-
-  //Функция, проверяющая выбран ли элемент для фильтра
-  const isFilterElement = (filterElement) => {
-    let isElement = true;
-    if (Array.isArray(features)) {
-      isElement = features.some(element => filterElement.value === element);
+  //Функция, возращающая значение поля выбора цены
+  const priceValue = () => {
+    if (price < Price_Rank.LOW[1]) {
+      return Price_Rank.LOW[0];
     }
-
-    return filterElement.checked && isElement || !filterElement.checked;
+    if (price >= Price_Rank.LOW[1] && price <= Price_Rank.MIDDLE[1]) {
+      return Price_Rank.MIDDLE[0];
+    }
+    if (price > Price_Rank.MIDDLE[1]) {
+      return Price_Rank.HIGH[0];
+    }
   };
 
-  const isFilterWifi = isFilterElement(filterWifi);
-  const isFilterDishwasher = isFilterElement(filterDishwasher);
-  const isFilterParking = isFilterElement(filterParking);
-  const isFilterWasher = isFilterElement(filterWasher);
-  const isFilterElevator = isFilterElement(filterElevator);
-  const isFilterConditioner = isFilterElement(filterConditioner);
+  //Функции, проверяющая выбор значений для фильтра
+  const isFilterType = () => {
+    return housingType.value === Default_Value.HOUSING_TYPE || housingType.value === type;
+  };
 
-  return isHousingType
-      && isHousingPrice
-      && isHousingRooms 
-      && isHousingGuests 
-      && isFilterWifi 
-      && isFilterDishwasher 
-      && isFilterParking 
-      && isFilterWasher 
-      && isFilterElevator 
-      && isFilterConditioner;
+  const isFilterPrice = () => {
+    return housingPrice.value === Default_Value.HOUSING_PRICE || housingPrice.value === priceValue();
+  };
+
+  const isFilterRooms = () => {
+    return housingRooms.value === Default_Value.HOUSING_ROOMS || housingRooms.value === String(rooms);
+  };
+
+  const isFilterGuests = () => {
+    return housingGuests.value === Default_Value.HOUSING_GUEST || housingGuests.value === String(guests);
+  };
+
+  const isFilterFeatures = () => {
+    let isFilter = true;
+
+    filterFeatures.forEach(filterElement => {
+      let isElement = true;
+      if (Array.isArray(features)) {
+        isElement = features.some(element => filterElement.value === element);
+      }
+
+      let isFilterElement = filterElement.checked && isElement || !filterElement.checked;
+
+      isFilter =  isFilter && isFilterElement;    
+    });
+
+    return isFilter;
+  };
+
+  // const isHousingType = isFilterType();
+  // const isHousingPrice = isFilterPrice();
+  // const isHousingRooms = isFilterRooms();
+  // const isHousingGuests = isFilterGuests();
+  // const isHousingFeatures = isFilterFeatures();
+
+  // return isHousingType
+  //     && isHousingPrice
+  //     && isHousingRooms 
+  //     && isHousingGuests
+  //     && isHousingFeatures
+  
+  return isFilterType()
+      && isFilterPrice()
+      && isFilterRooms()
+      && isFilterGuests()
+      && isFilterFeatures()
+
 };
 
-export { 
-  filterPopup,
-  // onHousingGuestsChange,
-  // onHousingPriceChange,
-  // onHousingRoomsChange,
-  // onHousingTypeChange,
-  // onFilterConditionerClick,
-  // onFilterDishwasherClick,
-  // onFilterElevatorClick,
-  // onFilterParkingClick,
-  // onFilterWasherClick,
-  // onFilterWifiClick,
-  onFormMapFiltersChange
-};
+export { filterPopup, onFormMapFiltersChange };
--- source/js/main.js
+++ source/js/main.js
@@ -4,35 +4,13 @@ import { loadMap, renderPopups} from './map.js';
 import { getData } from './api.js';
 import { setUserFormSubmit } from './user-form.js';
 import { showAlert } from './util.js';
-import { 
-  // onHousingGuestsChange,
-  // onHousingPriceChange,
-  // onHousingRoomsChange,
-  // onHousingTypeChange,
-  // onFilterConditionerClick,
-  // onFilterDishwasherClick,
-  // onFilterElevatorClick,
-  // onFilterParkingClick,
-  // onFilterWasherClick,
-  // onFilterWifiClick,
-  onFormMapFiltersChange
-} from './filters.js';
+import { onFormMapFiltersChange} from './filters.js';
 
 const RERENDER_DELAY = 500;
 
 const loadPopup = ()=> {
   getData((cards) => {
     renderPopups(cards);
-    // onHousingTypeChange(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onHousingPriceChange(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onHousingRoomsChange(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onHousingGuestsChange(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onFilterWifiClick(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onFilterDishwasherClick(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onFilterParkingClick(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onFilterWasherClick(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onFilterElevatorClick(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
-    // onFilterConditionerClick(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
     onFormMapFiltersChange(_.debounce(() => renderPopups(cards), RERENDER_DELAY));
   },
   (error) => showAlert(`Упс... Ошибка загрузки данных... ${error}`),
